a86a80dadf31f7318fa173e52c492d727568d264
Dany Roy <daniel.rojek7@gmail.com>
2020-06-23 22:54:55 +0200

    Dodanie DbUp

---
+++ BFinances.DbUp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using DbUp;
+using Microsoft.Extensions.Configuration;
+
+namespace BFinances.DbUp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Development.json");
+
+            var configuration = builder.Build();
+
+            // TODO: Dodać environment
+            var connectionString = configuration.GetConnectionString("InvoicesDatabase");
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
---
+++ BFinances.DbUp/Scripts/001_InitDb.sql
@@ -0,0 +1,32 @@
+﻿USE [BFinances]
+
+CREATE TABLE [dbo].[Contractor](
+	[Name] [varchar](200) NOT NULL,
+	[Nip] [varchar](10) NOT NULL,
+	[Id] [bigint] IDENTITY(1,1) NOT NULL
+)
+GO
+
+CREATE TABLE [dbo].[Pkwiu](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Code] [varchar](30) NOT NULL,
+	[Name] [varchar](200) NOT NULL
+)
+GO
+
+CREATE TABLE [dbo].[Invoice](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Number] [varchar](50) NOT NULL,
+	[FromContractorId] [bigint] NOT NULL,
+	[ForContractorId] [bigint] NOT NULL,
+	[InvoiceDate] [datetime] NOT NULL,
+	[DueDate] [datetime] NOT NULL,
+	[SaleDate] [datetime] NOT NULL,
+	[NetAmount] [decimal](18, 0) NOT NULL,
+	[NetCurrency] [varchar](3) NOT NULL,
+	[VatPercent] [decimal](18, 0) NOT NULL,
+	[NumberOfUnits] [int] NOT NULL,
+	[UnitName] [varchar](20) NOT NULL,
+	[PkwiuId] [bigint] NOT NULL
+)
+GO
