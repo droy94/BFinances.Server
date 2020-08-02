6bb8f9c39348f971a7e403f1e300b94d776b31ef
Dany Roy <daniel.rojek7@gmail.com>
2020-08-02 22:30:21 +0200

    Poprawa skryptu

--- BFinances.DbUp/Scripts/002_AddExpensesTable.sql
+++ BFinances.DbUp/Scripts/002_AddExpensesTable.sql
@@ -1,12 +1,14 @@
 ﻿CREATE TABLE [dbo].[Expenses](
 	[Id] [bigint] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[Name] [varchar](500) NOT NULL,
 	[ExpenseNo] [varchar](50) NOT NULL,
 	[FromContractorId] [bigint] FOREIGN KEY REFERENCES Contractors(Id) NOT NULL,
 	[ForContractorId] [bigint] FOREIGN KEY REFERENCES Contractors(Id) NOT NULL,
 	[ExpenseDate] [datetime] NOT NULL,
 	[DueDate] [datetime] NOT NULL,
-	[NetSum] [decimal](18, 0) NOT NULL,
+	[VatAmount] [decimal](18, 0) NOT NULL,
+	[NetAmount] [decimal](18, 0) NOT NULL,
 	[GrossSum] [decimal](18, 0) NOT NULL,
-	[VatSum] [decimal](18, 0) NOT NULL
+	[VatPercent] [decimal](18, 0) NOT NULL
 )
 GO
