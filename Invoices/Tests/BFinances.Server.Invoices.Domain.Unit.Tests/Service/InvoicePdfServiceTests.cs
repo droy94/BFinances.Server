fe640d79738695476cd2ed6fe0e3e13b152bf23e
Dany Roy <daniel.rojek7@gmail.com>
2020-08-23 13:34:47 +0200

    Dodanie testu dla invoicePdfService

---
+++ Invoices/Tests/BFinances.Server.Invoices.Domain.Unit.Tests/Service/InvoicePdfServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using BFinances.Server.Invoices.Contract.Service;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BFinances.Server.Invoices.Contract.Providers;
+using BFinances.Server.Invoices.Contract.Response;
+using BFinances.Server.Invoices.Domain.Service;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using Xunit;
+
+namespace BFinances.Server.Invoices.Domain.Unit.Tests.Service
+{
+    public class InvoicePdfServiceTests
+    {
+        [Fact]
+        public async Task Generate_OnProperParameter_CallsConcreteMethods()
+        {
+            // Arrange
+            var templateGeneratorMock = new Mock<IInvoiceTemplateGenerator>();
+            var invoicesProviderMock = new Mock<IInvoicesProvider>();
+            var pdfConverterMock = new Mock<IConverter>();
+
+            var invoicePdfService =
+                new InvoicePdfService(templateGeneratorMock.Object, invoicesProviderMock.Object, pdfConverterMock.Object);
+
+            // Act
+            await invoicePdfService.Generate(1);
+
+            // Assert
+            templateGeneratorMock.Verify(mock => mock.GetContent(It.IsAny<InvoiceResponse>()), Times.Once);
+            invoicesProviderMock.Verify(mock => mock.GetInvoices(It.IsAny<long>()), Times.Once);
+            pdfConverterMock.Verify(mock => mock.Convert(It.IsAny<HtmlToPdfDocument>()), Times.Once);
+        }
+    }
+}
